d157c03c221a6b6aefe829ac028e88ad9173f257
laziztssa <laziztessa2@gmail.com>
2025-05-29 21:58:36 +0200

    modifs des path des notebook

--- Workflow/Dag_Velib.py
+++ Workflow/Dag_Velib.py
@@ -14,7 +14,7 @@ CLUSTER_NAME = "paris-opendata-cluster"
 COMPOSER_BUCKET = "us-east1-paris-opendata-com-baec303d-bucket" ## change this 
 
 
-GCS_JOB_FILE_1 = f"gs://{COMPOSER_BUCKET}/data/Workspace/Velib/Bronze_Velib_Emplacement_Stations_CSV.py"
+GCS_JOB_FILE_1 = f"gs://{COMPOSER_BUCKET}/data/scripts/Workspace/Velib/Bronze_Velib_Emplacement_Stations_CSV.py"
 JOB_1 = {
     "reference": {"project_id": PROJECT_ID},
     "placement": {"cluster_name": CLUSTER_NAME},
@@ -22,7 +22,7 @@ JOB_1 = {
 }
 
 
-GCS_JOB_FILE_2 = f"gs://{COMPOSER_BUCKET}/data/Workspace/Velib/Silver_Velib_Emplacement_Stations.ipynb"
+GCS_JOB_FILE_2 = f"gs://{COMPOSER_BUCKET}/data/scripts/Workspace/Velib/Silver_Velib_Emplacement_Stations.ipynb"
 JOB_2 = {
     "reference": {"project_id": PROJECT_ID},
     "placement": {"cluster_name": CLUSTER_NAME},
@@ -32,7 +32,7 @@ JOB_2 = {
 
 
 
-GCS_JOB_FILE_3 = f"gs://{COMPOSER_BUCKET}/data/Workspace/Velib/Graphique_Emplacement_Station.ipynb"
+GCS_JOB_FILE_3 = f"gs://{COMPOSER_BUCKET}/data/scripts/Workspace/Velib/Graphique_Emplacement_Station.ipynb"
 JOB_3 = {
     "reference": {"project_id": PROJECT_ID},
     "placement": {"cluster_name": CLUSTER_NAME},
@@ -48,7 +48,7 @@ ARGS = {
     "depends_on_past": False,
     "email_on_failure": False,
     "email_on_retry": False,
-    "email": ["***@gmail.com"],
+    "email": ["laziztessa@gmail.com"],
     "email_on_success": False,
     "retries": 1,
     "retry_delay": timedelta(minutes=5)
